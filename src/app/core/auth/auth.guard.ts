8c1497550f27691a40c32eaa681fabe99e796bed
[Vini] <[vantoninicad@protonmail.com]>
2020-01-26 00:38:18 -0500

    final version pc02c03

---
+++ src/app/core/auth/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+import { Observable } from "rxjs";
+
+import { UserService } from "../user/user.service";
+
+@Injectable({providedIn:'root'})
+export class AuthGaurd implements CanActivate{
+  
+  constructor(
+    private userServices: UserService,
+    private router: Router
+    ) { }
+  
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean>  {
+      
+        if (this.userServices.isLogged()){
+          this.router.navigate(['user', this.userServices.getUserName()])
+          return false;
+        }
+      return true; // this is important otherwise we won't have access to the logout function
+    } 
+}
