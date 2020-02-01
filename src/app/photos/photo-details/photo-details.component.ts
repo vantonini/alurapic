7d5d3f231b1c7d9d7c6db76d2246f50ebff89e00
[Vini] <[vantoninicad@protonmail.com]>
2020-02-01 15:12:07 -0500

    final version p03c05

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Observable } from "rxjs";
+
 import { PhotoService } from "../photo/photo.service";
 import { Photo } from "../photo/photo";
-import { Observable } from "rxjs";
 
 @Component({
   // selector: 'ap-photo-details', // page scope, I don't need to include
@@ -14,6 +15,7 @@ export class PhotoDetailsComponent implements OnInit {
   photoId: number;
   
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
     private photoService: PhotoService
     ) { }
@@ -21,5 +23,10 @@ export class PhotoDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.photoId = this.route.snapshot.params.photoId;
     this.photo$ = this.photoService.findById(this.photoId);
+  }
+
+  remove() {
+    this.photoService.removePhoto(this.photoId)
+    .subscribe(() => this.router.navigate(['']));
   }
 }
